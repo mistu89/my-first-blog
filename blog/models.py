0e8f3097ae78222538cbad66f8dae9cd624d226f
marta <zdechlypet@gmail.com>
2020-02-02 19:08:29 +0100

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200) #definiujemy tekst z ograniczoną liczbą znaków
+    text = models.TextField() #text bez ograniczen
+    created_date = models.DateTimeField(
+        default=timezone.now) #data i godzina
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+    
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    
+    def __str__(self):
+        return self.title
